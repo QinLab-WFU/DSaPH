c775dbd006000a482ad8a1a192bf0b03555ed325
QinLab-WFU <128210944+QinLab-WFU@users.noreply.github.com>
2026-02-06 17:05:47 +0800

    Delete zoom.py

--- zoom.py
+++
@@ -1,7 +0,0 @@
-import xlrd
-import math
-num_bits = 16
-num_classes = 10
-
-sheet = xlrd.open_workbook('codetable.xls').sheet_by_index(0)
-threshold = sheet.row(num_bits)[math.ceil(math.log(num_classes, 2))].value
